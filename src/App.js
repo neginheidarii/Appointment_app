67d2dd7922e87c47608fc8a37a49836baba11a7a
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-08-05 19:45:44 -0400

    installing tailwind

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { SlCalender } from "react-icons/sl";
+
+function App() {
+  return (
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-4xl">
+        <SlCalender />
+        Your Appointments
+      </h1>
+    </div>
+  );
+}
+
+export default App;
