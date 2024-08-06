1d4f39c1b9d1dc25ec1e4a0f7388423fa7b32584
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-08-05 20:03:08 -0400

    added search and add appointment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { SlCalender } from "react-icons/sl";
+import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment";
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-4xl">
-        <SlCalender />
+        <SlCalender className="inline-block text-red-400 align-top mr-3" />
         Your Appointments
       </h1>
+
+      <Search />
+      <AddAppointment />
     </div>
   );
 }
