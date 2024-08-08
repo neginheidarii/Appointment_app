b5a22f30ec9a1f1ed4efb67e3a0738c84b1b5863
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-08-08 13:19:45 -0400

    completed add appointment functions

--- src/components/AppointmentInfo.js
+++ src/components/AppointmentInfo.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { BiTrash } from 'react-icons/bi';
 
-function AppointmentInfo({appointment}) {
+function AppointmentInfo({ appointment, onDeleteAppointment }) {
   return (
     <li className="px-3 py-3 flex items-start">
       <button
+        onClick={() => {
+          onDeleteAppointment(appointment.id);
+        }}
         type="button"
         className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
       >
